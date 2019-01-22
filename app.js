b4603d95f85ddde576ec68f914af0caa57d2544e
Andelko <aldeko96@gmail.com>
2019-01-22 14:46:21 +0100

    eredbhas

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-const row = document.createElement('div');
-row.className = 'row-js';
+let x = document.getElementById("sec1").childElementCount;
+let y = document.getElementById("sec2").childElementCount;
 
-const parent = document.querySelector('.row-wraper');
-let color = 0;
 
-for (let i = 0; i < 8; i++) {
-    const row = document.createElement('div');
-    row.className = 'row-js';
+let a = [];
+for (let i = 0; i < x; i++) {
+    a.push(i);
+}
 
-    for(let i = 0; i < 8; i ++){
-        const light = document.createElement('div');
-        
-            light.className = 'light-js';
-        row.appendChild(light);
-        console.log(color);
-        
-        color++;
+let b = [];
+for (i = 0; i < y; i++) {
+    b.push(i);
+}
+
+function shfl(array) {
+    for (let g = array.length - 1; g > 0; g--) {
+        const j = Math.floor(Math.random() *(g + 1));
+        [array[g], array[j]] = [array[j], array[g]];
     }
+}
+shfl(a);
+shfl(b);
 
-    
-    parent.appendChild(row);
+let items = a.concat(b);
+
+
+let all = document.getElementsByTagName("div");
+
+for (let d = 0, max=all.length; d < max; d++) {
+    all[d].setAttribute("id", items[d]);
+}
+console.log(items);
+
+let displayCard = function() {
+    //let e = document.getElementById(this);
+    this.innerHTML = this.id;
+    console.log(this.id);
 }
 
 
+
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+for (i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", displayCard);
+}
