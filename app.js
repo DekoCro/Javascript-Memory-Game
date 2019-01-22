9534c6306e109876248550ae56b054bfbde06d5c
AleSpreafico <ale.spreafico@yandex.com>
2019-01-22 19:11:28 +0100

    here it is

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let x = document.getElementById("sec1").childElementCount;
 let y = document.getElementById("sec2").childElementCount;
 
+const cards = document.querySelectorAll('.card');
 
 let a = [];
 for (let i = 0; i < x; i++) {
@@ -29,19 +30,65 @@ let all = document.getElementsByTagName("div");
 for (let d = 0, max=all.length; d < max; d++) {
     all[d].setAttribute("id", items[d]);
 }
-console.log(items);
 
-let displayCard = function() {
-    //let e = document.getElementById(this);
-    this.innerHTML = this.id;
-    console.log(this.id);
+let first = null;
+let second = null;
+
+function pick() {
+
+    if (this === first) return;
+    
+    if (first === null) {
+        this.innerHTML = this.id;
+        first = this;
+    } else if (second === null) {
+        this.innerHTML = this.id;
+        second = this;
+        check();
+    }
+}
+
+function check() {
+    if (first.id === second.id) {
+        deletePicks();
+        return;
+    }
+
+    setTimeout(unflip, 500);
+}
+
+function unflip() {
+    first.innerHTML = " ";
+    second.innerHTML = " ";
+    resetFirstSecond();
 }
 
 
+function deletePicks() {
+    first.classList.add('disabled');
+    first.removeEventListener('click', pick);
+    second.classList.add('disabled');
+    second.removeEventListener('click', pick);
+    resetFirstSecond();
+}
 
-let card = document.getElementsByClassName("card");
-let cards = [...card];
+function resetFirstSecond() {
+    first = null;
+    second = null;
+}
+
 
 for (i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", displayCard);
+    cards[i].addEventListener("click", pick);
 }
+
+// const displayCard = function() {
+//     //let e = document.getElementById(this);
+//     this.innerHTML = this.id;
+//     count++;
+//     // console.log(this.id);
+// }
+
+// const hideCard = function() {
+//     this.innerHTML = "";
+// }
